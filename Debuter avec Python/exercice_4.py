348d53bee18dba2dd300412cc2276e159c3d2703
Anis <anisa@lyceechurchill.london>
2024-09-24 14:35:59 +0000

    exercice_4

---
+++ Debuter avec Python/exercice_4.py
@@ -0,0 +1,17 @@
+import random
+
+c = random.randint(1,100)
+P = c * 4
+A = c ** 2
+c = 7
+
+def perimetre(x):
+    return 4 * x
+P = c * 4
+print("Le périmètre du carré est :", P)
+
+
+def surface(x):
+    return x * x
+A = c ** 2
+print("La surface du carre est :", A)
